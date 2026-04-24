aeb179505d0cba65ebac91b89eebad6a7fa3ce5e
Basel-shibob <shibobbasel@gmail.com>
2026-04-24 17:35:04 +0300

    Lab 3: file persistence for add and list

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const task = [];
+const fs = require("fs");
+const path = require("path");
+
+const FILE = path.join(__dirname, "tasks.json");
+
+
+function loadTasks(){
+	if (fs.existsSync(FILE)){
+		let reads = fs.readFileSync(FILE, "utf8");
+		return JSON.parse(reads);
+	}else{
+		return [];
+	}
+}
+
+function saveTasks(tasks){
+	return fs.writeFileSync(FILE, JSON.stringify(tasks, null, 2));
+}
+
+
 const args = process.argv.slice(2);
 
 
@@ -9,15 +28,18 @@ console.log("Usage: node index.js <add|list> [text]")
 
 }else{
 	if (args[0] === "add"){
-		const newTask = {id: task.length + 1, text: args[1], done:false};
-		task.push(newTask);
-		console.log("Task added:", task);
+		let tasks = loadTasks()
+		const newTask = {id:tasks.length + 1, text: args[1] , done:false}
+		tasks.push(newTask);
+		saveTasks(tasks)
+		console.log("Task added:", tasks);
 	}
 	if (args[0] === "list"){
-		if (task.length === 0){
+		let tasks = loadTasks()
+		if (tasks.length === 0){
 			console.log("No tasks yet")
 		}else{
-			console.log(task);
+			console.log(tasks);
 		}	
 	}
 }
